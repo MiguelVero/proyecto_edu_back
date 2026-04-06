f002caad8cd2f4b793a012a0dd95957ff45bfe59
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-04-06 00:52:54 -0500

    correcion de config en orden

--- src/routes/ordenRoutes.js
+++ src/routes/ordenRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs-extra');
+const config = require('../config/config');
 const { 
     obtenerOrdenes, 
     obtenerOrdenPorId,
