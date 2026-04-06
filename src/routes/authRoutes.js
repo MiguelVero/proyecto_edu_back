b3d86c7f00f7fdfb2db0cc3761135033d4c9c16a
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-04-06 15:49:34 -0500

    comentario

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const { login, verificarToken, cambiarContrasena } = require('../controllers/authController');
 const { autenticar } = require('../middleware/auth');
 const { validarLogin } = require('../middleware/validator');
-
+// Rutas de autenticación
 router.post('/login', validarLogin, login);
 router.get('/verificar', autenticar, verificarToken);
 router.post('/cambiar-contrasena', autenticar, cambiarContrasena);
-
 module.exports = router;
